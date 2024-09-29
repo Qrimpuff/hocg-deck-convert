8aa4110df693d0f1cee814f67dac2735ebc3af5b
Qrimpuff <89057495+Qrimpuff@users.noreply.github.com>
2024-09-29 18:31:41 +0000

    Deploying to gh-pages from @ Qrimpuff/hocg-deck-convert@fd7582c84e953e29d53e375b9dc48983fe6faa81 🚀

--- assets/dioxus/hocg-deck-convert.js
+++ assets/dioxus/hocg-deck-convert.js
@@ -1555,15 +1555,15 @@ function __wbg_get_imports() {
         return addHeapObject(ret);
     };
     imports.wbg.__wbindgen_closure_wrapper1559 = function() { return logError(function (arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 757, __wbg_adapter_48);
+        const ret = makeMutClosure(arg0, arg1, 751, __wbg_adapter_48);
         return addHeapObject(ret);
     }, arguments) };
     imports.wbg.__wbindgen_closure_wrapper1907 = function() { return logError(function (arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 862, __wbg_adapter_51);
+        const ret = makeMutClosure(arg0, arg1, 856, __wbg_adapter_51);
         return addHeapObject(ret);
     }, arguments) };
     imports.wbg.__wbindgen_closure_wrapper2118 = function() { return logError(function (arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 933, __wbg_adapter_54);
+        const ret = makeMutClosure(arg0, arg1, 927, __wbg_adapter_54);
         return addHeapObject(ret);
     }, arguments) };
 
